02a6c666b5e246b15dc908a213051a21ee361b99
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-14 18:49:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,8 +51,9 @@ class Song
 
  def self.new_from_filename(mp3_input)
      info = mp3_input.split(" - ")
-     @name = info[1].tr(".mp3")
-     @artist_name = info[0]
+     name = info[1].tr(".mp3")
+     artist_name = info[0]
+
      new_song = self.new
      new_song.name = name
      new_song.artist_name = artist_name
