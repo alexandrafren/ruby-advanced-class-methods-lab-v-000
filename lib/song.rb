aec4ca80b47ba21a4a22b6adfb4505e7c94ac659
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-14 18:00:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
   end
 
   def self.find_or_create_by_name(song_name)
-    if find_by_name(song_name)
+    if self.find_by_name(song_name)
     else
       create_by_name(song_name)
     end
